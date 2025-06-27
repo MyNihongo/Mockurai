c3e79a370b1d76ff1e5e8af198a84cbf52b98a58
kuzmins <kuzmins@vesca.co.jp>
2025-06-27 09:13:20 +0900

    tests for

--- sample/MyNihongo.Mock.Sample/_Generated/SetupWithSeveralParametersTests/InvokePrimitiveShould.cs
+++ sample/MyNihongo.Mock.Sample/_Generated/SetupWithSeveralParametersTests/InvokePrimitiveShould.cs
@@ -3,6 +3,80 @@ namespace MyNihongo.Mock.Sample._Generated.SetupWithSeveralParametersTests;
 public sealed class InvokePrimitiveShould : SetupWithSeveralParametersTestsBase
 {
 	[Fact]
+	public void PrioritiseValueOverAnyThrow1()
+	{
+		const int setupValue1 = 1, setupValue2 = 2;
+
+		const string errorMessage1 = nameof(errorMessage1);
+		It<int> setup11 = It<int>.Any(), setup12 = It<int>.Any();
+
+		const string errorMessage2 = nameof(errorMessage2);
+		It<int> setup21 = setupValue1, setup22 = default;
+
+		const string errorMessage3 = nameof(errorMessage3);
+		It<int> setup31 = default, setup32 = setupValue2;
+
+		const string errorMessage4 = nameof(errorMessage4);
+		It<int> setup41 = setupValue1, setup42 = setupValue2;
+
+		var fixture = CreateFixture<SetupIntInt>();
+		fixture.SetupParameters(setup11, setup12);
+		fixture.Throws(new InvalidOperationException(errorMessage1));
+
+		fixture.SetupParameters(setup21, setup22);
+		fixture.Throws(new ArgumentException(errorMessage2));
+
+		fixture.SetupParameters(setup31, setup32);
+		fixture.Throws(new InvalidCastException(errorMessage3));
+
+		fixture.SetupParameters(setup41, setup42);
+		fixture.Throws(new OverflowException(errorMessage4));
+
+		const int input2 = 324343242;
+		var actual = () => fixture.Invoke(setupValue1, input2);
+
+		var exception = Assert.Throws<ArgumentException>(actual);
+		Assert.Equal(errorMessage2, exception.Message);
+	}
+
+	[Fact]
+	public void PrioritiseValueOverAnyThrow2()
+	{
+		const int setupValue1 = 1, setupValue2 = 2;
+
+		const string errorMessage1 = nameof(errorMessage1);
+		It<int> setup11 = It<int>.Any(), setup12 = It<int>.Any();
+
+		const string errorMessage2 = nameof(errorMessage2);
+		It<int> setup21 = setupValue1, setup22 = default;
+
+		const string errorMessage3 = nameof(errorMessage3);
+		It<int> setup31 = default, setup32 = setupValue2;
+
+		const string errorMessage4 = nameof(errorMessage4);
+		It<int> setup41 = setupValue1, setup42 = setupValue2;
+
+		var fixture = CreateFixture<SetupIntInt>();
+		fixture.SetupParameters(setup11, setup12);
+		fixture.Throws(new InvalidOperationException(errorMessage1));
+
+		fixture.SetupParameters(setup21, setup22);
+		fixture.Throws(new ArgumentException(errorMessage2));
+
+		fixture.SetupParameters(setup31, setup32);
+		fixture.Throws(new InvalidCastException(errorMessage3));
+
+		fixture.SetupParameters(setup41, setup42);
+		fixture.Throws(new OverflowException(errorMessage4));
+
+		const int input1 = 324343242;
+		var actual = () => fixture.Invoke(input1, setupValue2);
+
+		var exception = Assert.Throws<InvalidCastException>(actual);
+		Assert.Equal(errorMessage3, exception.Message);
+	}
+
+	[Fact]
 	public void PrioritiseValueOverAnyThrow12()
 	{
 		const int setupValue1 = 1, setupValue2 = 2;
@@ -36,5 +110,42 @@ public sealed class InvokePrimitiveShould : SetupWithSeveralParametersTestsBase
 
 		var exception = Assert.Throws<OverflowException>(actual);
 		Assert.Equal(errorMessage4, exception.Message);
+	}
+
+	[Fact]
+	public void PrioritiseValueOverAnyThrowAny()
+	{
+		const int setupValue1 = 1, setupValue2 = 2;
+
+		const string errorMessage1 = nameof(errorMessage1);
+		It<int> setup11 = It<int>.Any(), setup12 = It<int>.Any();
+
+		const string errorMessage2 = nameof(errorMessage2);
+		It<int> setup21 = setupValue1, setup22 = default;
+
+		const string errorMessage3 = nameof(errorMessage3);
+		It<int> setup31 = default, setup32 = setupValue2;
+
+		const string errorMessage4 = nameof(errorMessage4);
+		It<int> setup41 = setupValue1, setup42 = setupValue2;
+
+		var fixture = CreateFixture<SetupIntInt>();
+		fixture.SetupParameters(setup11, setup12);
+		fixture.Throws(new InvalidOperationException(errorMessage1));
+
+		fixture.SetupParameters(setup21, setup22);
+		fixture.Throws(new ArgumentException(errorMessage2));
+
+		fixture.SetupParameters(setup31, setup32);
+		fixture.Throws(new InvalidCastException(errorMessage3));
+
+		fixture.SetupParameters(setup41, setup42);
+		fixture.Throws(new OverflowException(errorMessage4));
+
+		const int input1 = 324343242, input2 = 837483252;
+		var actual = () => fixture.Invoke(input1, input2);
+
+		var exception = Assert.Throws<InvalidOperationException>(actual);
+		Assert.Equal(errorMessage1, exception.Message);
 	}
 }
