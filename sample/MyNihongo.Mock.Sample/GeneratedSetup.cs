58abd0a6a697a168e055e0dfc958bbe590909e43
kuzmins <kuzmins@vesca.co.jp>
2025-07-14 16:31:01 +0900

    callback for Setup

--- sample/MyNihongo.Mock.Sample/GeneratedSetup.cs
+++ sample/MyNihongo.Mock.Sample/GeneratedSetup.cs
@@ -16,11 +16,19 @@ public interface ISetup<in T> : ISetup
 public sealed class Setup : ISetup
 {
 	private Exception? _exception;
+	private Action? _callback;
 
 	public void Invoke()
 	{
+		_callback?.Invoke();
+
 		if (_exception is not null)
 			throw _exception;
+	}
+
+	public void Callback(in Action callback)
+	{
+		_callback = callback;
 	}
 
 	public void Throws(in Exception exception)
