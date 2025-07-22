14eb9c4564e271eedfc2948b97192c55fcecd5f3
kuzmins <kuzmins@vesca.co.jp>
2025-07-22 18:21:02 +0900

    move collections

--- src/MyNihongo.Mock.Abstractions/Collections/InvocationContainer.cs
+++ src/MyNihongo.Mock.Abstractions/Collections/InvocationContainer.cs
@@ -1,49 +1,8 @@
 using System.Collections;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-namespace MyNihongo.Mock.Sample;
+namespace MyNihongo.Mock;
 
-[Obsolete("Will be generated")]
-public sealed class SetupContainer<T> : IEnumerable<T>
-{
-	private readonly IComparer<T> _comparer;
-	private readonly List<T> _setups = [];
-
-	public SetupContainer(IComparer<T> comparer)
-	{
-		_comparer = comparer;
-	}
-
-	public void Add(in T item)
-	{
-		var index = _setups.BinarySearch(item, _comparer);
-
-		if (index < 0)
-		{
-			index = ~index;
-		}
-		else
-		{
-			var span = CollectionsMarshal.AsSpan(_setups);
-			while (index < span.Length && _comparer.Compare(item, span[index]) == 0)
-				index++;
-		}
-
-		_setups.Insert(index, item);
-	}
-
-	public IEnumerator<T> GetEnumerator()
-	{
-		for (var i = _setups.Count - 1; i >= 0; i--)
-			yield return _setups[i];
-	}
-
-	IEnumerator IEnumerable.GetEnumerator() =>
-		GetEnumerator();
-}
-
-[Obsolete("Will be generated")]
 public sealed class InvocationContainer<T> : IEnumerable<(long Index, T Invocation)>
 {
 	private readonly List<(long Index, T Invocation)> _invocations = [];
@@ -107,13 +66,5 @@ public sealed class InvocationContainer<T> : IEnumerable<(long Index, T Invocati
 	{
 		for (var i = 0; i < _invocations.Count; i++)
 			yield return _invocations[i].GetString();
-	}
-}
-
-public static class TupleEx
-{
-	public static string GetString<T>(this (long, T) @this)
-	{
-		return $"{@this.Item1}: {@this.Item2}";
 	}
 }
