f7b28d90a4f612b5de01998077cde5a3cb63647a
kuzmins <kuzmins@vesca.co.jp>
2025-07-28 11:58:36 +0900

    add tests for It

--- src/MyNihongo.Mock.Abstractions/Comparison/Equivalent/EquivalencyComparer.cs
+++ src/MyNihongo.Mock.Abstractions/Comparison/Equivalent/EquivalencyComparer.cs
@@ -1,8 +1,24 @@
+using System.Collections.Frozen;
+using System.Reflection;
+
 namespace MyNihongo.Mock;
 
 public sealed class EquivalencyComparer<T>
 {
 	public static readonly EquivalencyComparer<T> Default = new();
+	private readonly FrozenDictionary<string, PropertyInfo> _properties;
+	private readonly FrozenDictionary<string, FieldInfo> _fields;
+
+	private EquivalencyComparer()
+	{
+		_properties = typeof(T)
+			.GetProperties(BindingFlags.Instance | BindingFlags.Public)
+			.ToFrozenDictionary(static x => x.Name);
+
+		_fields = typeof(T)
+			.GetFields(BindingFlags.Instance | BindingFlags.Public)
+			.ToFrozenDictionary(static x => x.Name);
+	}
 
 	public bool Equivalent(T? x, T? y)
 	{
---
+++ tests/MyNihongo.Mock.Abstractions.Tests/Core/ItTests/ValueShould.cs
@@ -0,0 +1,30 @@
+namespace MyNihongo.Mock.Abstractions.Tests.Core.ItTests;
+
+public sealed class ValueShould
+{
+	[Fact]
+	public void BeTrueForSameValue()
+	{
+		const int inputValue = 123;
+
+		var actual = It<int>.Value(inputValue)
+			.ValueSetup!.Value
+			.Predicate(inputValue);
+
+		Assert.True(actual);
+	}
+
+	[Theory]
+	[InlineData(320)]
+	[InlineData(322)]
+	public void BeFalseForDifferentValue(int inputValue)
+	{
+		const int setupValue = 321;
+
+		var actual = It<int>.Value(setupValue)
+			.ValueSetup!.Value
+			.Predicate(inputValue);
+
+		Assert.False(actual);
+	}
+}
