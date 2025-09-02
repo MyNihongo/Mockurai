ba09fa0150a2f0bbf78a965771d81901d53d7012
kuzmins <kuzmins@vesca.co.jp>
2025-09-02 11:34:57 +0900

    undo ref

--- src/MyNihongo.Mock.Abstractions/Setup/SetupWithParameter.cs
+++ src/MyNihongo.Mock.Abstractions/Setup/SetupWithParameter.cs
@@ -1,7 +1,11 @@
 namespace MyNihongo.Mock;
 
-public sealed class SetupWithParameter<TParameter> : SetupWithParameterBase<TParameter, Action<TParameter>>
+public sealed class SetupWithParameter<TParameter> : ISetup
 {
+	private static readonly Comparer SortComparer = new();
+	private SetupContainer<Item>? _setups;
+	private Item? _currentSetup;
+
 	public void Invoke(in TParameter parameter)
 	{
 		if (_setups is null)
@@ -18,35 +22,6 @@ public sealed class SetupWithParameter<TParameter> : SetupWithParameterBase<TPar
 				throw setup.Exception;
 		}
 	}
-}
-
-public sealed class SetupWithRefParameter<TParameter> : SetupWithParameterBase<TParameter, ActionRef<TParameter>>
-{
-	public void Invoke(ref TParameter parameter)
-	{
-		if (_setups is null)
-			return;
-
-		foreach (var setup in _setups)
-		{
-			if (setup.Parameter.HasValue && !setup.Parameter.Value.Check(parameter))
-				continue;
-
-			setup.Callback?.Invoke(ref parameter);
-
-			if (setup.Exception is not null)
-				throw setup.Exception;
-		}
-	}
-}
-
-public abstract class SetupWithParameterBase<TParameter, TDelegate> : ISetup
-{
-	private static readonly Comparer SortComparer = new();
-
-	// ReSharper disable once InconsistentNaming
-	protected SetupContainer<Item>? _setups;
-	private Item? _currentSetup;
 
 	public void SetupParameter(in It<TParameter> parameter)
 	{
@@ -56,7 +31,7 @@ public abstract class SetupWithParameterBase<TParameter, TDelegate> : ISetup
 		_setups.Add(_currentSetup);
 	}
 
-	public void Callback(in TDelegate callback)
+	public void Callback(in Action<TParameter> callback)
 	{
 		if (_currentSetup is null)
 			throw new InvalidOperationException("Parameters are not set, call SetupParameters first!");
@@ -72,10 +47,10 @@ public abstract class SetupWithParameterBase<TParameter, TDelegate> : ISetup
 		_currentSetup.Exception = exception;
 	}
 
-	protected sealed class Item(in It<TParameter>.Setup? parameter)
+	private sealed class Item(in It<TParameter>.Setup? parameter)
 	{
 		public readonly It<TParameter>.Setup? Parameter = parameter;
-		public TDelegate? Callback;
+		public Action<TParameter>? Callback;
 		public Exception? Exception;
 	}
 
