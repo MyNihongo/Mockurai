fa336a042951978dd9d502817dfdefa7716d2ea2
kuzmins <kuzmins@vesca.co.jp>
2025-09-03 09:51:17 +0900

    start tests for ref params

---
+++ tests/MyNihongo.Mock.Abstractions.Tests/Setup/SetupWithRefParameterTests/InvokeClassShould.cs
@@ -0,0 +1,27 @@
+namespace MyNihongo.Mock.Abstractions.Tests.Setup.SetupWithRefParameterTests;
+
+public sealed class InvokeClassShould : SetupWithRefParameterTestsBase
+{
+	[Fact]
+	public void ThrowForAnySetup()
+	{
+		const string errorMessage = nameof(errorMessage);
+		var setup = It<ClassParameter1>.Any();
+
+		var fixture = CreateFixture(setup);
+		fixture.Throws(new InvalidOperationException(errorMessage));
+
+		var input = new ClassParameter1
+		{
+			Text = "any text",
+			Number = 12345678,
+		};
+
+		var actual = () => fixture.Invoke(ref input);
+
+		var exception = Assert.Throws<InvalidOperationException>(actual);
+		Assert.Equal(errorMessage, exception.Message);
+	}
+	
+	
+}
---
+++ tests/MyNihongo.Mock.Abstractions.Tests/Setup/SetupWithRefParameterTests/InvokeRecordShould.cs
@@ -0,0 +1,24 @@
+namespace MyNihongo.Mock.Abstractions.Tests.Setup.SetupWithRefParameterTests;
+
+public sealed class InvokeRecordShould : SetupWithRefParameterTestsBase
+{
+	[Fact]
+	public void ThrowForAnySetup()
+	{
+		const string errorMessage = nameof(errorMessage);
+		var setup = It<RecordParameter1>.Any();
+
+		var fixture = CreateFixture(setup);
+		fixture.Throws(new InvalidOperationException(errorMessage));
+
+		var input = new RecordParameter1(
+			Text: "any text",
+			Number: 12345678
+		);
+
+		var actual = () => fixture.Invoke(ref input);
+
+		var exception = Assert.Throws<InvalidOperationException>(actual);
+		Assert.Equal(errorMessage, exception.Message);
+	}
+}
