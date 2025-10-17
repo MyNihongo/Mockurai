2005ce4327d5441aff2298977846f95d6d5d0576
kuzmins <kuzmins@vesca.co.jp>
2025-10-17 17:26:32 +0900

    tests for Add

---
+++ tests/MyNihongo.Mock.Abstractions.Tests/Collections/InvocationContainerTests/InvocationContainerTestsBase.cs
@@ -0,0 +1,23 @@
+namespace MyNihongo.Mock.Abstractions.Tests.Collections.InvocationContainerTests;
+
+public abstract class InvocationContainerTestsBase
+{
+	protected static InvocationContainer<Invocation> CreateFixture()
+	{
+		return [];
+	}
+
+	protected sealed class Invocation : IInvocation
+	{
+		public long Index { get; init; }
+
+		public string? Label { get; init; }
+
+		public override string ToString()
+		{
+			return !string.IsNullOrEmpty(Label)
+				? $"{Index}: {Label}"
+				: Index.ToString();
+		}
+	}
+}
