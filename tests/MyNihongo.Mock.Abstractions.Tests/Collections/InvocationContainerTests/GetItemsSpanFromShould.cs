d0530f0df259d25d39e0f8846d6c096a545f2b84
kuzmins <kuzmins@vesca.co.jp>
2025-10-17 17:42:50 +0900

    tests for span from

--- tests/MyNihongo.Mock.Abstractions.Tests/Collections/InvocationContainerTests/GetItemsSpanFromShould.cs
+++ tests/MyNihongo.Mock.Abstractions.Tests/Collections/InvocationContainerTests/GetItemsSpanFromShould.cs
@@ -1,12 +1,12 @@
 namespace MyNihongo.Mock.Abstractions.Tests.Collections.InvocationContainerTests;
 
-public sealed class TryGetItemAtShould : InvocationContainerTestsBase
+public sealed class GetItemsSpanFromShould : InvocationContainerTestsBase
 {
 	[Theory]
 	[InlineData(0)]
 	[InlineData(1)]
 	[InlineData(2)]
-	public void ReturnNextElementStart(int index)
+	public void ReturnEntireCollectionFromStart(int index)
 	{
 		var fixture = CreateFixture();
 		fixture.Add(new Invocation { Index = 2 });
@@ -14,24 +14,36 @@ public sealed class TryGetItemAtShould : InvocationContainerTestsBase
 		fixture.Add(new Invocation { Index = 7 });
 		fixture.Add(new Invocation { Index = 10 });
 
-		var actual = fixture.TryGetItemAt(index);
+		var actual = fixture.GetItemsSpanFrom(index)
+			.ToArray();
 
-		var expected = new Invocation { Index = 2 };
+		var expected = new Invocation[]
+		{
+			new() { Index = 2 },
+			new() { Index = 3 },
+			new() { Index = 7 },
+			new() { Index = 10 },
+		};
 		Assert.Equivalent(expected, actual);
 	}
 
 	[Fact]
-	public void ReturnNextElementMiddle()
+	public void ReturnSliceFromMiddle()
 	{
 		var fixture = CreateFixture();
-		fixture.Add(new Invocation { Index = 1 });
+		fixture.Add(new Invocation { Index = 2 });
 		fixture.Add(new Invocation { Index = 3 });
 		fixture.Add(new Invocation { Index = 7 });
 		fixture.Add(new Invocation { Index = 10 });
 
-		var actual = fixture.TryGetItemAt(index: 4);
+		var actual = fixture.GetItemsSpanFrom(index: 4)
+			.ToArray();
 
-		var expected = new Invocation { Index = 7 };
+		var expected = new Invocation[]
+		{
+			new() { Index = 7 },
+			new() { Index = 10 },
+		};
 		Assert.Equivalent(expected, actual);
 	}
 
@@ -39,7 +51,7 @@ public sealed class TryGetItemAtShould : InvocationContainerTestsBase
 	[InlineData(8)]
 	[InlineData(9)]
 	[InlineData(10)]
-	public void ReturnNextElementEnd(int index)
+	public void ReturnLastElement(int index)
 	{
 		var fixture = CreateFixture();
 		fixture.Add(new Invocation { Index = 1 });
@@ -47,24 +59,13 @@ public sealed class TryGetItemAtShould : InvocationContainerTestsBase
 		fixture.Add(new Invocation { Index = 7 });
 		fixture.Add(new Invocation { Index = 10 });
 
-		var actual = fixture.TryGetItemAt(index);
+		var actual = fixture.GetItemsSpanFrom(index)
+			.ToArray();
 
-		var expected = new Invocation { Index = 10 };
+		var expected = new Invocation[]
+		{
+			new() { Index = 10 },
+		};
 		Assert.Equivalent(expected, actual);
-	}
-
-	[Theory]
-	[InlineData(11)]
-	[InlineData(100)]
-	public void ReturnNullIfNotFound(int index)
-	{
-		var fixture = CreateFixture();
-		fixture.Add(new Invocation { Index = 1 });
-		fixture.Add(new Invocation { Index = 3 });
-		fixture.Add(new Invocation { Index = 7 });
-		fixture.Add(new Invocation { Index = 10 });
-
-		var actual = fixture.TryGetItemAt(index);
-		Assert.Null(actual);
 	}
 }
