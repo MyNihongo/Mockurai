b42629c31daf0058cd0ae429bdc3574fbd4fe096
kuzmins <kuzmins@vesca.co.jp>
2025-11-05 11:40:49 +0900

    adjust BaseReturns

--- src/MyNihongo.Mock.Abstractions/Setup/Base/SetupBaseReturns.cs
+++ src/MyNihongo.Mock.Abstractions/Setup/Base/SetupBaseReturns.cs
@@ -1,49 +1,58 @@
 namespace MyNihongo.Mock;
 
-public abstract class SetupBaseReturns<TCallback, TReturns, TReturnsCallback> : ISetup<TCallback, TReturns, TReturnsCallback>
+public abstract class SetupBaseReturns<TCallback, TReturns, TReturnsCallback>
+	: ISetupCallbackJoin<TCallback, TReturns, TReturnsCallback>, ISetupCallback<TCallback, TReturns, TReturnsCallback>,
+		ISetupReturnsThrowsJoin<TCallback, TReturns, TReturnsCallback>, ISetupReturnsThrows<TCallback, TReturns, TReturnsCallback>
 {
 	private readonly Queue<ItemSetup> _queue = [];
-	private ItemSetup? _currentSetup;
+	private ItemSetup? _currentItem;
+	private bool _andContinue;
 
-	public abstract ISetup<TCallback, TReturns, TReturnsCallback> Returns(TReturns? returns);
+	public abstract void Returns(TReturns? returns);
 
-	public ISetup<TCallback, TReturns, TReturnsCallback> Callback(in TCallback callback)
+	public void Callback(in TCallback callback)
 	{
-		var currentSetup = new ItemSetup(callback);
-		_queue.Enqueue(currentSetup);
-		_currentSetup = currentSetup;
-
-		return this;
-	}
-
-	public ISetup<TCallback, TReturns, TReturnsCallback> Returns(in TReturnsCallback returns)
-	{
-		if (_currentSetup is null)
+		if (_andContinue && _currentItem is not null)
 		{
-			_queue.Enqueue(new ItemSetup(returns: returns));
+			_currentItem.Callback = callback;
+			_andContinue = false;
+			_currentItem = null;
 		}
 		else
 		{
-			_currentSetup.Returns = returns;
-			_currentSetup = null;
+			_currentItem = new ItemSetup(callback);
+			_queue.Enqueue(_currentItem);
 		}
-
-		return this;
 	}
 
-	public ISetup<TCallback, TReturns, TReturnsCallback> Throws(in Exception exception)
+	public void Returns(in TReturnsCallback returns)
 	{
-		if (_currentSetup is null)
+		if (_andContinue && _currentItem is not null)
 		{
-			_queue.Enqueue(new ItemSetup(exception: exception));
+			_currentItem.Returns = returns;
+			_andContinue = false;
+			_currentItem = null;
 		}
 		else
 		{
-			_currentSetup.Exception = exception;
-			_currentSetup = null;
+			_currentItem = new ItemSetup(returns: returns);
+			_queue.Enqueue(_currentItem);
 		}
+	}
 
-		return this;
+	public void Throws(in Exception exception)
+	{
+		if (_andContinue && _currentItem is not null)
+		{
+			_currentItem.Exception = exception;
+			_andContinue = false;
+			_currentItem = null;
+		}
+		else
+		{
+			_currentItem = new ItemSetup(exception: exception);
+			_queue.Enqueue(_currentItem);
+		}
 	}
 
 	protected ItemSetup GetSetup()
@@ -56,11 +65,71 @@ public abstract class SetupBaseReturns<TCallback, TReturns, TReturnsCallback> : 
 		};
 	}
 
+	ISetupCallbackJoin<TCallback, TReturns, TReturnsCallback> ISetupCallbackChain<TCallback, TReturns, TReturnsCallback>.Callback(in TCallback callback)
+	{
+		Callback(callback);
+		return this;
+	}
+
+	ISetup<TCallback, TReturns, TReturnsCallback> ISetupCallback<TCallback, TReturns, TReturnsCallback>.Callback(in TCallback callback)
+	{
+		Callback(callback);
+		return this;
+	}
+
+	ISetupReturnsThrowsJoin<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrowsChain<TCallback, TReturns, TReturnsCallback>.Throws(in Exception exception)
+	{
+		Throws(exception);
+		return this;
+	}
+
+	ISetup<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrows<TCallback, TReturns, TReturnsCallback>.Throws(in Exception exception)
+	{
+		Throws(exception);
+		return this;
+	}
+
+	ISetupReturnsThrowsJoin<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrowsChain<TCallback, TReturns, TReturnsCallback>.Returns(in TReturns returns)
+	{
+		Returns(returns);
+		return this;
+	}
+
+	ISetupReturnsThrowsJoin<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrowsChain<TCallback, TReturns, TReturnsCallback>.Returns(in TReturnsCallback returns)
+	{
+		Returns(returns);
+		return this;
+	}
+
+	ISetup<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrows<TCallback, TReturns, TReturnsCallback>.Returns(in TReturns returns)
+	{
+		Returns(returns);
+		return this;
+	}
+
+	ISetup<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrows<TCallback, TReturns, TReturnsCallback>.Returns(in TReturnsCallback returns)
+	{
+		Returns(returns);
+		return this;
+	}
+
+	ISetupReturnsThrows<TCallback, TReturns, TReturnsCallback> ISetupCallbackJoin<TCallback, TReturns, TReturnsCallback>.And()
+	{
+		_andContinue = true;
+		return this;
+	}
+
+	ISetupCallback<TCallback, TReturns, TReturnsCallback> ISetupReturnsThrowsJoin<TCallback, TReturns, TReturnsCallback>.And()
+	{
+		_andContinue = true;
+		return this;
+	}
+
 	protected sealed class ItemSetup(in TCallback? callback = default, in TReturnsCallback? returns = default, in Exception? exception = null)
 	{
 		public static readonly ItemSetup Default = new();
 
-		public readonly TCallback? Callback = callback;
+		public TCallback? Callback = callback;
 		public TReturnsCallback? Returns = returns;
 		public Exception? Exception = exception;
 	}
