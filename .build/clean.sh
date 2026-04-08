ae1b4b20dd2e6e216bb1bf2fc1f1cc429e08b99b
MihailsKuzmins <47413092+MihailsKuzmins@users.noreply.github.com>
2026-04-08 09:58:49 +0900

    create NuGet packages (#75)

---
+++ .build/clean.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+start_dir="$(pwd)"
+cd ../ || exit 1
+
+# Ensure we always return to original directory
+trap 'cd "$start_dir"' EXIT
+
+find . -type f -name "*.nupkg" | while read -r file; do
+	rm "$file"
+	echo "Deleted $file"
+done
