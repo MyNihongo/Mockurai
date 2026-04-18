ee504561b85147529d9280e5c0f54d9fd4ae30d8
kuzmins <kuzmins@vesca.co.jp>
2026-04-18 14:27:32 +0900

    add tests

---
+++ sample/MyNihongo.Mockurai.Sample/PrimitiveTypeServiceTests/DerivedTests/VerifyInSequenceShould.cs
@@ -0,0 +1,39 @@
+﻿namespace MyNihongo.Mockurai.Sample.PrimitiveTypeServiceTests.DerivedTests;
+
+public sealed class VerifyInSequenceShould : PrimitiveTypeServiceTestsDerivedBase
+{
+	[Fact]
+	public void VerifyInvocationInOrder()
+	{
+		DependencyServiceDerivedMock.Object.Invoke();
+		DependencyServiceMock.Object.Invoke();
+
+		VerifyInSequence(x =>
+		{
+			x.DependencyServiceDerivedMock.Invoke();
+			x.DependencyServiceMock.Invoke();
+		});
+	}
+
+	[Fact]
+	public void ThrowIfNotInOrder()
+	{
+		DependencyServiceDerivedMock.Object.Invoke();
+		DependencyServiceMock.Object.Invoke();
+
+		var actual = () => VerifyInSequence(x =>
+		{
+			x.DependencyServiceMock.Invoke();
+			x.DependencyServiceDerivedMock.Invoke();
+		});
+
+		const string errorMessage =
+			"""
+			Expected IPrimitiveDependencyService.Invoke() to be invoked at index 3, but it has not been called.
+			Performed invocations:
+			- 1: IPrimitiveDependencyService.Invoke()
+			""";
+		var exception = Assert.Throws<MockVerifySequenceOutOfRangeException>(actual);
+		Assert.Equal(errorMessage, exception.Message);
+	}
+}
