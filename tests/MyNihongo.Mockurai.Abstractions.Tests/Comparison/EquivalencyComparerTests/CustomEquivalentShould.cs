fb8f197e38771daff13570363601c7757ba0a96c
kuzmins <kuzmins@vesca.co.jp>
2026-04-24 18:39:06 +0900

    start tests

---
+++ tests/MyNihongo.Mockurai.Abstractions.Tests/Comparison/EquivalencyComparerTests/CustomEquivalentShould.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace MyNihongo.Mockurai.Abstractions.Tests.Comparison.EquivalencyComparerTests;
+
+public sealed class CustomEquivalentShould
+{
+	[Fact]
+	public void BeEmptyForEqualObjects()
+	{
+		var input1 = new { prop = "aaa" }.ToJsonElement();
+		var input2 = new { prop = "aaa" }.ToJsonElement();
+
+		It<JsonElement>.Equivalent(input1, new JsonElementComparer())
+			.ValueSetup.Check(input2, out var actual);
+
+		Assert.NotNull(actual);
+		Assert.Empty(actual.Entries);
+	}
+}
+
+file sealed class JsonElementComparer : IEquivalencyComparer<JsonElement>
+{
+	public ComparisonResult Equivalent(JsonElement x, JsonElement y)
+	{
+		var result = new ComparisonResult();
+		throw new NotImplementedException();
+	}
+}
+
+file static class Extensions
+{
+	public static JsonElement ToJsonElement<T>(this T @this)
+	{
+		return JsonSerializer.SerializeToElement(@this);
+	}
+}
