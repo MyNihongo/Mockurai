6872861ea2678c38fb01ce57b0bf5f96b7e5a6fd
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2026-04-25 21:47:13 +0900

    tests for generic

--- tests/MyNihongo.Mockurai.Tests/Issues/Inheritance.cs
+++ tests/MyNihongo.Mockurai.Tests/Issues/Inheritance.cs
@@ -986,7 +986,9 @@ public sealed class Inheritance : IssuesTestsBase
 
 			public interface IInterface1<T>
 			{
-				void Invoke1<T>();
+				void Invoke1(T value);
+
+				T Return();
 			}
 
 			public interface IInterface : IInterface1<decimal>
@@ -1001,7 +1003,230 @@ public sealed class Inheritance : IssuesTestsBase
 			}
 			""";
 
-		GeneratedSources generatedSources = [];
+		GeneratedSources generatedSources =
+		[
+			GetTestsBaseSource(),
+			(
+				"InterfaceMock.g.cs",
+				"""
+				#nullable enable
+				namespace MyNihongo.Mockurai;
+
+				public sealed class InterfaceMock : IMock<Issues.Tests.IInterface>
+				{
+					private readonly InvocationIndex.Counter _invocationIndex;
+					private readonly System.Func<System.Collections.Generic.IEnumerable<IInvocationProvider?>> _invocationProviders;
+					private Proxy? _proxy;
+
+					public InterfaceMock(InvocationIndex.Counter invocationIndex)
+					{
+						_invocationIndex = invocationIndex;
+						_invocationProviders = GetInvocations;
+					}
+
+					public Issues.Tests.IInterface Object => _proxy ??= new Proxy(this);
+
+					public InvocationContainer Invocations => field ??= new InvocationContainer(this);
+
+					// Invoke
+					private Setup? _invoke0;
+					private Invocation? _invoke0Invocation;
+
+					public Setup SetupInvoke()
+					{
+						_invoke0 ??= new Setup();
+						return _invoke0;
+					}
+
+					public void VerifyInvoke(in Times times)
+					{
+						_invoke0Invocation ??= new Invocation("IInterface.Invoke()");
+						_invoke0Invocation.Verify(times, _invocationProviders);
+					}
+
+					public long VerifyInvoke(long index)
+					{
+						_invoke0Invocation ??= new Invocation("IInterface.Invoke()");
+						return _invoke0Invocation.Verify(index, _invocationProviders);
+					}
+
+					// Invoke1
+					private SetupWithParameter<decimal>? _invoke10;
+					private Invocation<decimal>? _invoke10Invocation;
+
+					public SetupWithParameter<decimal> SetupInvoke1(in It<decimal> value)
+					{
+						_invoke10 ??= new SetupWithParameter<decimal>();
+						_invoke10.SetupParameter(value.ValueSetup);
+						return _invoke10;
+					}
+
+					public void VerifyInvoke1(in It<decimal> value, in Times times)
+					{
+						_invoke10Invocation ??= new Invocation<decimal>("IInterface.Invoke1({0})");
+						_invoke10Invocation.Verify(value.ValueSetup, times, _invocationProviders);
+					}
+
+					public long VerifyInvoke1(in It<decimal> value, long index)
+					{
+						_invoke10Invocation ??= new Invocation<decimal>("IInterface.Invoke1({0})");
+						return _invoke10Invocation.Verify(value.ValueSetup, index, _invocationProviders);
+					}
+
+					// Return
+					private Setup<decimal>? _return0;
+					private Invocation? _return0Invocation;
+
+					public Setup<decimal> SetupReturn()
+					{
+						_return0 ??= new Setup<decimal>();
+						return _return0;
+					}
+
+					public void VerifyReturn(in Times times)
+					{
+						_return0Invocation ??= new Invocation("IInterface.Return()");
+						_return0Invocation.Verify(times, _invocationProviders);
+					}
+
+					public long VerifyReturn(long index)
+					{
+						_return0Invocation ??= new Invocation("IInterface.Return()");
+						return _return0Invocation.Verify(index, _invocationProviders);
+					}
+
+					public void VerifyNoOtherCalls()
+					{
+						_invoke0Invocation?.VerifyNoOtherCalls(_invocationProviders);
+						_invoke10Invocation?.VerifyNoOtherCalls(_invocationProviders);
+						_return0Invocation?.VerifyNoOtherCalls(_invocationProviders);
+					}
+
+					private System.Collections.Generic.IEnumerable<IInvocationProvider?> GetInvocations()
+					{
+						yield return _invoke0Invocation;
+						yield return _invoke10Invocation;
+						yield return _return0Invocation;
+					}
+
+					private sealed class Proxy : Issues.Tests.IInterface
+					{
+						private readonly InterfaceMock _mock;
+
+						public Proxy(InterfaceMock mock)
+						{
+							_mock = mock;
+						}
+
+						public void Invoke()
+						{
+							_mock._invoke0Invocation ??= new Invocation("IInterface.Invoke()");
+							_mock._invoke0Invocation.Register(_mock._invocationIndex);
+							_mock._invoke0?.Invoke();
+						}
+
+						public void Invoke1(decimal value)
+						{
+							_mock._invoke10Invocation ??= new Invocation<decimal>("IInterface.Invoke1({0})");
+							_mock._invoke10Invocation.Register(_mock._invocationIndex, value);
+							_mock._invoke10?.Invoke(value);
+						}
+
+						public decimal Return()
+						{
+							_mock._return0Invocation ??= new Invocation("IInterface.Return()");
+							_mock._return0Invocation.Register(_mock._invocationIndex);
+							return _mock._return0?.Execute(out var returnValue) == true ? returnValue! : default!;
+						}
+					}
+
+					public sealed class InvocationContainer
+					{
+						private readonly InterfaceMock _mock;
+
+						public InvocationContainer(InterfaceMock mock)
+						{
+							_mock = mock;
+						}
+
+						public System.Collections.Generic.IEnumerable<IInvocation> Invoke => _mock._invoke0Invocation?.GetInvocations() ?? [];
+
+						public System.Collections.Generic.IEnumerable<IInvocation<decimal>> Invoke1 => _mock._invoke10Invocation?.GetInvocationsWithArguments() ?? [];
+
+						public System.Collections.Generic.IEnumerable<IInvocation> Return => _mock._return0Invocation?.GetInvocations() ?? [];
+					}
+				}
+
+				public static partial class MockExtensions
+				{
+					extension(IMock<Issues.Tests.IInterface> @this)
+					{
+						public InterfaceMock.InvocationContainer Invocations => ((InterfaceMock)@this).Invocations;
+
+						public void VerifyNoOtherCalls() =>
+							((InterfaceMock)@this).VerifyNoOtherCalls();
+
+						// Invoke
+						public ISetup<System.Action> SetupInvoke() =>
+							((InterfaceMock)@this).SetupInvoke();
+
+						public void VerifyInvoke(in Times times) =>
+							((InterfaceMock)@this).VerifyInvoke(times);
+
+						public void VerifyInvoke(System.Func<Times> times) =>
+							((InterfaceMock)@this).VerifyInvoke(times());
+
+						// Invoke1
+						public ISetup<System.Action<decimal>> SetupInvoke1(in It<decimal> value = default) =>
+							((InterfaceMock)@this).SetupInvoke1(value);
+
+						public void VerifyInvoke1(in It<decimal> value, in Times times) =>
+							((InterfaceMock)@this).VerifyInvoke1(value, times);
+
+						public void VerifyInvoke1(in It<decimal> value, System.Func<Times> times) =>
+							((InterfaceMock)@this).VerifyInvoke1(value, times());
+
+						// Return
+						public ISetup<System.Action, decimal, System.Func<decimal>> SetupReturn() =>
+							((InterfaceMock)@this).SetupReturn();
+
+						public void VerifyReturn(in Times times) =>
+							((InterfaceMock)@this).VerifyReturn(times);
+
+						public void VerifyReturn(System.Func<Times> times) =>
+							((InterfaceMock)@this).VerifyReturn(times());
+					}
+				}
+
+				public static partial class MockSequenceExtensions
+				{
+					extension(IMockSequence<Issues.Tests.IInterface> @this)
+					{
+						// Invoke
+						public void Invoke()
+						{
+							var nextIndex = ((InterfaceMock)@this.Mock).VerifyInvoke(@this.VerifyIndex);
+							@this.VerifyIndex.Set(nextIndex);
+						}
+
+						// Invoke1
+						public void Invoke1(in It<decimal> value)
+						{
+							var nextIndex = ((InterfaceMock)@this.Mock).VerifyInvoke1(value, @this.VerifyIndex);
+							@this.VerifyIndex.Set(nextIndex);
+						}
+
+						// Return
+						public void Return()
+						{
+							var nextIndex = ((InterfaceMock)@this.Mock).VerifyReturn(@this.VerifyIndex);
+							@this.VerifyIndex.Set(nextIndex);
+						}
+					}
+				}
+				"""
+			),
+		];
 
 		var ctx = CreateFixture(testCode, generatedSources);
 		await ctx.RunAsync();
