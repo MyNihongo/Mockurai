968a828c385fc49d3e801b982c86f45f78e8862a
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2026-05-03 11:42:11 +0900

    add some tests

--- tests/MyNihongo.Mockurai.Tests/Utils/ClassInheritanceTreeTests/Base/ClassInheritanceTreeTestsBase.cs
+++ tests/MyNihongo.Mockurai.Tests/Utils/ClassInheritanceTreeTests/Base/ClassInheritanceTreeTestsBase.cs
@@ -1,20 +1,45 @@
 using System.Collections.Immutable;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using MyNihongo.Mockurai.Utils;
 
 namespace MyNihongo.Mockurai.Tests.Utils.ClassInheritanceTreeTests;
 
-public abstract class ClassInheritanceTreeTestsBase
+public abstract partial class ClassInheritanceTreeTestsBase
 {
-	protected IEnumerable<INamedTypeSymbol> CreateFixture(ImmutableArray<TransformResult?> items)
+	protected static IEnumerable<string> CreateFixture(string input, CancellationToken ct = default)
 	{
-		return new ClassInheritanceTree<TransformResult>(items, null!)
-			.Select(x => x.GetNamedTypeSymbol(null!)!);
+		var compilation = CreateCompilation(input, ct);
+
+		var items = ClassRegex().Matches(input)
+			.Where(x => x.Success)
+			.Select(x => x.Value)
+			.Select(TransformResult? (x) => new TransformResult(x))
+			.ToImmutableArray();
+
+		return new ClassInheritanceTree<TransformResult>(items, compilation)
+			.Select(x => x.GetNamedTypeSymbol(compilation)?.Name!);
 	}
 
-	protected sealed class TransformResult(INamedTypeSymbol namedTypeSymbol) : ITransformResult
+	private sealed class TransformResult(string name) : ITransformResult
 	{
-		public INamedTypeSymbol GetNamedTypeSymbol(Compilation compilation) =>
-			namedTypeSymbol;
+		public INamedTypeSymbol? GetNamedTypeSymbol(Compilation compilation) =>
+			compilation.GetTypeByMetadataName(name);
+
+		public override string ToString() =>
+			name;
 	}
+
+	private static CSharpCompilation CreateCompilation(string input, CancellationToken ct = default)
+	{
+		return CSharpCompilation.Create(
+			assemblyName: "ClassInheritanceTreeTestsBase",
+			syntaxTrees: [CSharpSyntaxTree.ParseText(input, cancellationToken: ct)],
+			references: [MetadataReference.CreateFromFile(typeof(object).Assembly.Location)]
+		);
+	}
+
+	[GeneratedRegex(@"(?<=\bclass\s)Class\S*", RegexOptions.Multiline)]
+	private static partial Regex ClassRegex();
 }
--- tests/MyNihongo.Mockurai.Tests/Utils/ClassInheritanceTreeTests/ToArrayShould.cs
+++ tests/MyNihongo.Mockurai.Tests/Utils/ClassInheritanceTreeTests/ToArrayShould.cs
@@ -2,5 +2,88 @@ namespace MyNihongo.Mockurai.Tests.Utils.ClassInheritanceTreeTests;
 
 public sealed class ToArrayShould : ClassInheritanceTreeTestsBase
 {
-	
+	[Fact]
+	public void ReturnTwoRootNodes()
+	{
+		const string input =
+			"""
+			public class Class1 {}
+			public class Class2 {}
+			""";
+
+		var actual = CreateFixture(input, TestContext.Current.CancellationToken)
+			.ToArray();
+
+		string[] expected = ["Class1", "Class2"];
+		Assert.Equivalent(expected, actual, true);
+	}
+
+	[Fact]
+	public void ReturnParentChild()
+	{
+		const string input =
+			"""
+			public class Class1 : Class2 {}
+			public class Class2 {}
+			""";
+
+		var actual = CreateFixture(input, TestContext.Current.CancellationToken)
+			.ToArray();
+
+		string[] expected = ["Class2", "Class1"];
+		Assert.Equivalent(expected, actual, true);
+	}
+
+	[Fact]
+	public void ReturnParentChildren()
+	{
+		const string input =
+			"""
+			public class Class1 : Class2 {}
+			public class Class2 {}
+			public class Class3 : Class2 {}
+			""";
+
+		var actual = CreateFixture(input, TestContext.Current.CancellationToken)
+			.ToArray();
+
+		string[] expected = ["Class2", "Class1", "Class3"];
+		Assert.Equivalent(expected, actual, true);
+	}
+
+	[Fact]
+	public void ReturnParentChildChild()
+	{
+		const string input =
+			"""
+			public class Class1 : Class3 {}
+			public class Class2 {}
+			public class Class3 : Class2 {}
+			""";
+
+		var actual = CreateFixture(input, TestContext.Current.CancellationToken)
+			.ToArray();
+
+		string[] expected = ["Class2", "Class3", "Class1"];
+		Assert.Equivalent(expected, actual, true);
+	}
+
+	[Fact]
+	public void ReturnRootDerived()
+	{
+		const string input =
+			"""
+			public class Parent
+			public class Class1 : Class2 {}
+			public class Class2 {}
+			public class Class3 : Parent {}
+			public class Class4 {}
+			""";
+
+		var actual = CreateFixture(input, TestContext.Current.CancellationToken)
+			.ToArray();
+
+		string[] expected = ["Class2", "Class4", "Class3", "Class1"];
+		Assert.Equivalent(expected, actual, true);
+	}
 }
