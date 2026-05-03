56979b112c673c33f3d5950009954272d732a3bc
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2026-05-03 20:05:03 +0900

    more tests

--- tests/MyNihongo.Mockurai.Abstractions.Tests/Comparison/EquivalencyComparerTests/EquivalentTupleShould.cs
+++ tests/MyNihongo.Mockurai.Abstractions.Tests/Comparison/EquivalencyComparerTests/EquivalentTupleShould.cs
@@ -26,6 +26,56 @@ public sealed class EquivalentTupleShould
 		};
 		Assert.Equivalent(expected, actual.Entries, true);
 	}
+
+	[Fact]
+	public void CompareOnlyComparableValid()
+	{
+		var input1 = ("text", new OnlyComparable("key1"));
+		var input2 = ("text", new OnlyComparable("key1"));
+
+		var actual = EquivalencyComparer<(string, OnlyComparable)>.Default.Equivalent(input1, input2);
+		Assert.Empty(actual.Entries);
+	}
+
+	[Fact]
+	public void CompareOnlyComparableInvalid()
+	{
+		var input1 = ("text", new OnlyComparable("key1"));
+		var input2 = ("text", new OnlyComparable("key2"));
+
+		var actual = EquivalencyComparer<(string, OnlyComparable)>.Default.Equivalent(input1, input2);
+
+		var expected = new ComparisonResult.Entry[]
+		{
+			new("this", "(text, key1)", "(text, key2)"),
+		};
+		Assert.Equivalent(expected, actual.Entries, true);
+	}
+
+	[Fact]
+	public void CompareOnlyComparableGenericValid()
+	{
+		var input1 = ("text", new OnlyComparableGeneric("key1"));
+		var input2 = ("text", new OnlyComparableGeneric("key1"));
+
+		var actual = EquivalencyComparer<(string, OnlyComparableGeneric)>.Default.Equivalent(input1, input2);
+		Assert.Empty(actual.Entries);
+	}
+
+	[Fact]
+	public void CompareOnlyComparableGenericInvalid()
+	{
+		var input1 = ("text", new OnlyComparableGeneric("key1"));
+		var input2 = ("text", new OnlyComparableGeneric("key2"));
+
+		var actual = EquivalencyComparer<(string, OnlyComparableGeneric)>.Default.Equivalent(input1, input2);
+
+		var expected = new ComparisonResult.Entry[]
+		{
+			new("this", "(text, key1)", "(text, key2)"),
+		};
+		Assert.Equivalent(expected, actual.Entries, true);
+	}
 }
 
 file readonly struct OnlyEquatable(string key) : IEquatable<OnlyEquatable>
@@ -41,8 +91,34 @@ file readonly struct OnlyEquatable(string key) : IEquatable<OnlyEquatable>
 	public override int GetHashCode() =>
 		Key.GetHashCode();
 
-	public override string ToString()
+	public override string ToString() =>
+		Key;
+}
+
+file readonly struct OnlyComparable(string key) : IComparable
+{
+	public readonly string Key = key;
+
+	public int CompareTo(object? obj)
 	{
-		return Key;
+		return obj is OnlyComparable other
+			? Key.CompareTo(other.Key, StringComparison.InvariantCulture)
+			: 1;
 	}
+
+	public override string ToString() =>
+		Key;
+}
+
+file readonly struct OnlyComparableGeneric(string key) : IComparable<OnlyComparableGeneric>
+{
+	public readonly string Key = key;
+
+	public int CompareTo(OnlyComparableGeneric other)
+	{
+		return Key.CompareTo(other.Key, StringComparison.InvariantCulture);
+	}
+
+	public override string ToString() =>
+		Key;
 }
